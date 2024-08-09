ee88d5b40d23462800be9894db33cd5dbbd9c2cd
ravishashu <56905495+ravishashu@users.noreply.github.com>
2024-08-09 08:46:11 +0100

    Created DB context and add migration "Add-Migration InitialCreation -OutputDir Data/Migrations -Project Ordering.Infrastructure -StartupProject Ordering.API "

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Ordering.Infrastructure;
@@ -9,11 +10,14 @@ public static class DependencyInjection
     {
         var connectionString = configuration.GetConnectionString("Database");
 
-        //// Add services to the container.
-        //services.AddDbContext<ApplicationDbContext>(options =>
-        //    options.UseSqlServer(connectionString));
+       // services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+       // services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
 
-        //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+        services.AddDbContext<ApplicationDbContext>((sp, options) =>
+        {
+            options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+            options.UseSqlServer(connectionString);
+        });
 
         return services;
     }
