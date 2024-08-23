5e0027d9341138349ef3ac326f1c176e0be38005
ravishashu <56905495+ravishashu@users.noreply.github.com>
2024-08-23 08:52:42 +0100

    Pagination and getorder handler

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -4,3 +4,4 @@ global using BuildingBlocks.CQRS;
 global using Ordering.Application.Data;
 global using Ordering.Application.Dtos;
 global using Ordering.Application.Exceptions;
+global using Ordering.Application.Extensions;
