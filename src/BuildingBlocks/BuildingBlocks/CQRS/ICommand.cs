c5e70c12e6104177725ee269ffa5a6403b8f47d6
ravishashu <56905495+ravishashu@users.noreply.github.com>
2026-03-21 10:01:02 +0000

    session 1 buid catalog api

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    //Unit is a struct that represents a void type, it is used to indicate that a command does not return any data.
+    public interface ICommand : ICommand<Unit>
+    {
+    }
+
+    public interface ICommand<out TResponse> : IRequest<TResponse>
+    {
+    }
+}
